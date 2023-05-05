cfff54109050d7c45298ce8fe72021f8fbc7299d
Jon Jackson <jonjackson.webdesign@gmail.com>
2023-05-04 21:42:28 -0500

    added all the html pages to that folder so the design is now finished

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,6 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="bg-gray-100 min-h-screen w-screen">
-      <main className="max-w-screen-2xl m-auto bg-white">
-        {/* NAVBAR */}
-        <nav className="bg-white p-2 flex justify-between">
-          <a href="/" className="font-bold text-gray-700 text-2xl">
-            Open Table
-          </a>
-          <div>
-            <div className="flex">
-              <button className="bg-blue-400 text-white border p-1 px-4 rounded mr-3">
-                Sign in
-              </button>
-              <button className="border p-1 px-4 rounded">Sign up</button>
-            </div>
-          </div>
-        </nav>
-      </main>
-    </main>
+    <main></main>
   );
 }
