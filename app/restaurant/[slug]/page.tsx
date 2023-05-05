ec145d191717119c4db91bcc0679b9225f558cd5
Jon Jackson <jonjackson.webdesign@gmail.com>
2023-05-04 22:35:02 -0500

    basic navigation is done

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 const RestaurantDetails = () => {
@@ -6,9 +7,9 @@ const RestaurantDetails = () => {
   <main className="max-w-screen-2xl m-auto bg-white">
     {/* NAVBAR */}
     <nav className="bg-white p-2 flex justify-between">
-      <a href="" className="font-bold text-gray-700 text-2xl">
+      <Link href="/" className="font-bold text-gray-700 text-2xl">
         {" "} OpenTable{" "}
-      </a>
+      </Link>
       <div>
         <div className="flex">
           <button
@@ -35,8 +36,8 @@ const RestaurantDetails = () => {
       <div className="bg-white w-[70%] rounded p-3 shadow">
         {/* RESAURANT NAVBAR */}
         <nav className="flex text-reg border-b pb-2">
-          <a href="" className="mr-7"> Overview </a>
-          <a href="" className="mr-7"> Menu </a>
+          <Link href="/restaurant/milestone-grill" className="mr-7"> Overview </Link>
+          <Link href="/restaurant/milestone-grill/menu" className="mr-7"> Menu </Link>
         </nav>
         {/* RESAURANT NAVBAR */} {/* TITLE */}
         <div className="mt-4 border-b pb-6">
