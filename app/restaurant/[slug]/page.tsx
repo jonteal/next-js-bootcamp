0b550dc88574732c7c5694d816def0878cf38231
Jon Jackson <jonjackson.webdesign@gmail.com>
2023-05-07 14:16:06 -0500

    getting the dynamic restaurant from the db when it is clicked

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,5 +1,5 @@
+import { PrismaClient } from "@prisma/client";
 import Description from "./components/Description";
-import Header from "./components/Header";
 import Images from "./components/Images";
 import Rating from "./components/Rating";
 import ReservationCard from "./components/ReservationCard";
@@ -7,7 +7,42 @@ import RestaurantNavBar from "./components/RestaurantNavBar";
 import Reviews from "./components/Reviews";
 import Title from "./components/Title";
 
-const RestaurantDetails = () => {
+const prisma = new PrismaClient();
+
+interface Restaurant {
+  id: number;
+  name: string;
+  images: string[];
+  description: string;
+  slug: string;
+}
+
+const fetchRestaurantBySlug = async (slug: string): Promise<Restaurant> => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug,
+    },
+    select: {
+      id: true,
+      name: true,
+      images: true,
+      description: true,
+      slug: true,
+    }
+  });
+
+  if(!restaurant) {
+    throw new Error;
+  }
+
+  return restaurant;
+};
+
+const RestaurantDetails = async ({ params }: { params: { slug: string } }) => {
+  const restaurant = await fetchRestaurantBySlug(params.slug);
+
+  console.log('restaurant: ', restaurant);
+
   return (
     <>
       <div className="bg-white w-[70%] rounded p-3 shadow">
