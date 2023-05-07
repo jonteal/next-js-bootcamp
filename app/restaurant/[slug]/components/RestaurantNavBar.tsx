7a8b7383a2d5eabc0ad8c1db64d0b318ab45a268
Jon Jackson <jonjackson.webdesign@gmail.com>
2023-05-07 14:58:06 -0500

    all pages are dynamically showing their respective data

--- app/restaurant/[slug]/components/RestaurantNavBar.tsx
+++ app/restaurant/[slug]/components/RestaurantNavBar.tsx
@@ -6,9 +6,9 @@ const RestaurantNavBar = ({slug}: {slug: string}) => {
       <Link href={`/restaurant/${slug}`} className="mr-7">
         Overview
       </Link>
-      <Link href={`/restaurant/${slug}`} className="mr-7">
-        {" "}
-        Menu{" "}
+      <Link href={`/restaurant/${slug}/menu`} className="mr-7">
+      
+        Menu
       </Link>
     </nav>
   );
--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,13 +1,34 @@
-import Header from "../components/Header";
+import { PrismaClient } from "@prisma/client";
 import Menu from "../components/Menu";
 import RestaurantNavBar from "../components/RestaurantNavBar";
 
-const RestaurantMenu = () => {
+const prisma = new PrismaClient();
+
+const fetchRestaurantMenu =  async (slug: string) => {
+  const restaurant = await prisma.restaurant.findUnique({
+    where: {
+      slug
+    },
+    select: {
+      items: true
+    }
+  })
+
+  if(!restaurant) {
+    throw new Error;
+  }
+
+  return restaurant.items;
+}
+
+const RestaurantMenu = async ({ params }: { params: { slug: string } }) => {
+  const menu = await fetchRestaurantMenu(params.slug)
+  console.log('menu: ', menu);
   return (
     <>
       <div className="bg-white w-[100%] rounded p-3 shadow">
-        <RestaurantNavBar />
-        <Menu />
+        <RestaurantNavBar slug={params.slug} />
+        <Menu menu={menu} />
       </div>
     </>
   );
