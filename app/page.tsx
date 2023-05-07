497a5c141d7719a471a84048f70dea53dcc946e2
Jon Jackson <jonjackson.webdesign@gmail.com>
2023-05-07 12:47:45 -0500

    updated all restaurant cards with dynamic data

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,51 @@
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
+import { PrismaClient, Cuisine, Location, PRICE } from "@prisma/client";
 
-export default function Home() {
+export interface RestaurantCardType {
+  id: number;
+  name: string;
+  main_image: string;
+  cuisine: Cuisine;
+  location: Location;
+  price: PRICE;
+  slug: string;
+}
+
+const prisma = new PrismaClient();
+
+
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      slug: true,
+      location: true,
+      price: true
+    }
+  });
+
+  return restaurants;
+};
+
+const Home = async () => {
+  const restaurants = await fetchRestaurants();
+
+  console.log({ restaurants });
   return (
     <main>
       <Header />
       <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-        <RestaurantCard />
+        {restaurants.map(restaurant => (
+          <RestaurantCard restaurant={restaurant} />
+        ) )}
       </div>
     </main>
   );
-}
+};
 
-// 3Eg7OFcDUuG3mH7q
+export default Home;
