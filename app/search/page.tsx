4a4a998914ab838b077f95a8fe5e13b60696a516
Jon Jackson <jonjackson.webdesign@gmail.com>
2023-05-07 20:39:23 -0500

    started the query parameter filter logic

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,15 +1,82 @@
+import { PRICE, PrismaClient } from "@prisma/client";
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
 import SearchSidebar from "./components/SearchSidebar";
 
-const Search = () => {
+const prisma = new PrismaClient();
+
+const fetchRestaurantsByCity = async (city: string | undefined) => {
+  prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: "toronto"
+        }
+      },
+      cuisine: {
+        name: {
+          equals: "mexican"
+        }
+      },
+      price: {
+        equals: PRICE.CHEAP
+      }
+    }
+  })
+  const select = {
+    id: true,
+    name: true,
+    main_image: true,
+    price: true,
+    cuisine: true,
+    location: true,
+    slug: true,
+  };
+
+  if (!city) return prisma.restaurant.findMany({ select });
+
+  return prisma.restaurant.findMany({
+    where: {
+      location: {
+        name: {
+          equals: city.toLowerCase(),
+        },
+      },
+    },
+    select,
+  });
+};
+
+const fetchLocations = async () => {
+  return prisma.location.findMany();
+};
+
+const fetchCuisines = async () => {
+  return prisma.cuisine.findMany();
+};
+
+const Search = async ({
+  searchParams,
+}: {
+  searchParams: { city?: string; cuisine?: string; price?: PRICE };
+}) => {
+  const restaurants = await fetchRestaurantsByCity(searchParams.city);
+
+  const location = await fetchLocations();
+  const cuisine = await fetchCuisines();
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-        <SearchSidebar />
+        <SearchSidebar searchParams={searchParams} locations={location} cuisines={cuisine} />
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length ? (
+            restaurants.map((restaurant) => (
+              <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+            ))
+          ) : (
+            <p>Sorry, we found no restaurants available in this area.</p>
+          )}
         </div>
       </div>
     </>
