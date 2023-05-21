9dc57041412d8f96f23d9f7c44858938b5847ce5
Jon Jackson <jonjackson.webdesign@gmail.com>
2023-05-21 14:02:55 -0500

    updated modal to work for sign in and sign up

--- app/components/AuthModal.tsx
+++ app/components/AuthModal.tsx
@@ -23,6 +23,22 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
+  const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const [inputs, setInputs] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    city: "",
+    password: "",
+  });
+
   const renderContent = (signinContent: string, signupContent: string) => {
     return isSignin ? signinContent : signupContent;
   };
@@ -58,7 +74,11 @@ export default function AuthModal({ isSignin }: { isSignin: boolean }) {
                   "Create Your OpenTable Account"
                 )}
               </h2>
-              <AuthModalInputs />
+              <AuthModalInputs
+                isSignin={isSignin}
+                handleChangeInput={handleChangeInput}
+                inputs={inputs}
+              />
               <button className="uppercase bg-red-600 w-full text-white p-3 rounded text-sm mb-5 disabled:bg-gray-400">
                 {renderContent("Sign in", "Create Account")}
               </button>
--- app/components/AuthModalInputs.tsx
+++ app/components/AuthModalInputs.tsx
@@ -1,42 +1,77 @@
-const AuthModalInputs = () => {
+interface Props {
+  inputs: {
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone: string;
+    city: string;
+    password: string;
+  };
+  handleChangeInput: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  isSignin: boolean;
+}
+
+const AuthModalInputs = ({ inputs, handleChangeInput, isSignin }: Props) => {
   return (
     <div>
-      <div className="my-3 flex justify-between text-sm">
-        <input
-          placeholder="First Name"
-          type="text"
-          className="border rounded p-2 py-3 w-[49%]"
-        />
-        <input
-          placeholder="Last Name"
-          type="text"
-          className="border rounded p-2 py-3 w-[49%]"
-        />
-      </div>
+      {isSignin ? null : (
+        <div className="my-3 flex justify-between text-sm">
+          <input
+            placeholder="First Name"
+            type="text"
+            className="border rounded p-2 py-3 w-[49%]"
+            value={inputs.firstName}
+            onChange={handleChangeInput}
+            name="firstName"
+          />
+          <input
+            placeholder="Last Name"
+            type="text"
+            className="border rounded p-2 py-3 w-[49%]"
+            value={inputs.lastName}
+            onChange={handleChangeInput}
+            name="lastName"
+          />
+        </div>
+      )}
       <div className="my-3 flex justify-between text-sm">
         <input
           placeholder="Email"
           type="text"
           className="border rounded p-2 py-3 w-full"
+          value={inputs.email}
+          onChange={handleChangeInput}
+          name="email"
         />
       </div>
-      <div className="my-3 flex justify-between text-sm">
-        <input
-          placeholder="Phone"
-          type="text"
-          className="border rounded p-2 py-3 w-[49%]"
-        />
-        <input
-          placeholder="City"
-          type="text"
-          className="border rounded p-2 py-3 w-[49%]"
-        />
-      </div>
+      {isSignin ? null : (
+        <div className="my-3 flex justify-between text-sm">
+          <input
+            placeholder="Phone"
+            type="text"
+            className="border rounded p-2 py-3 w-[49%]"
+            value={inputs.phone}
+            onChange={handleChangeInput}
+            name="phone"
+          />
+          <input
+            placeholder="City"
+            type="text"
+            className="border rounded p-2 py-3 w-[49%]"
+            value={inputs.city}
+            onChange={handleChangeInput}
+            name="city"
+          />
+        </div>
+      )}
       <div className="my-3 flex justify-between text-sm">
         <input
           placeholder="Password"
-          type="text"
+          type="password"
           className="border rounded p-2 py-3 w-full"
+          value={inputs.password}
+          onChange={handleChangeInput}
+          name="password"
         />
       </div>
     </div>
